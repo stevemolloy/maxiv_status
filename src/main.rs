1be1ec175d067ee27c12a41666b719104dbd956f
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-05-23 22:16:18 +0200

    Got R3 current data out of the json
    serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use eventsource::reqwest::Client;
 use reqwest::Url;
+use serde_json::Value;
 
 fn main() {
-    const URL: &str = "http://status.maxiv.lu.se/stream";
-    let client = Client::new(Url::parse(URL).unwrap());
+    const STREAM_URL: &str = "http://status.maxiv.lu.se/stream";
+    let client = Client::new(Url::parse(STREAM_URL).unwrap());
 
     for event in client {
-        println!("{}", event.unwrap());
+        let data = &event.as_ref().unwrap().data;
+        let relevant = data.contains("R3-319S2/DIA/DCCT-01/CURRENT");
+        if relevant {
+            let v: Value = serde_json::from_str(data).unwrap();
+            let r3_info = v["R3-319S2/DIA/DCCT-01/CURRENT"]["value"].as_f64().unwrap();
+            println!("R3: {:.0} mA", r3_info * 1e3);
+        }
     }
 }
